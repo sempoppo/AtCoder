f7cb4267531d1c441ef959e6aeebc6505f7c5f60
Noah <72800797+thous4nd@users.noreply.github.com>
2021-08-04 18:16:34 +0900

    Add files via upload

---
+++ ABC/211/B.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int MOD = 1e9+7;
+const int dx[4] = { 1, 0, -1, 0};
+const int dy[4] = { 0, 1, 0, -1};
+
+template<class T> void chmax(T& a, T b) {
+    if(a < b) {
+        a = b;
+    }
+}
+
+struct UnionFind {
+    vector<int> par, siz;
+
+    // 初期化
+    UnionFind(int n) : par(n, -1), siz(n, 1) {}
+
+    // 根を求める
+    int root(int x){
+        if(par[x] == -1) return x;
+        else return par[x] = root(par[x]);
+    }
+
+    // x と y が同じグループに属するかどうか
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    // x を含むグループと y を含むグループとを併合する
+    bool unite(int x, int y) {
+        // x, y をそれぞれ根まで移動する
+        x = root(x), y = root(y);
+
+        // すでに同じグループのときは何もしない
+        if(x == y) return false;
+
+        // union by size (y 側のサイズが小さくなるようにする)
+        if(siz[x] < siz[y]) swap(x, y);
+
+        // y を x の子とする
+        par[y] = x;
+        siz[x] += siz[y];
+        return true;
+    }
+
+    // x を含むグループのサイズ
+    int size(int x){
+        return siz[root(x)];
+    }
+};
+	
+ll modPow(ll n, ll p, ll m){
+    if (p == 0) return 1;
+    if (p % 2 == 1) return (n * modPow(n, p - 1, m)) % m;
+    ll t = modPow(n, p / 2, m);
+    return (t * t) % m;
+}
+
+int my_gcd(int x, int y){
+    if(x%y == 0){
+        return y;
+    } else{
+        return my_gcd(y, x%y);
+    }
+}
+
+int main(){
+    vector<string> s(4);
+    rep(i,4) cin >> s[i];
+    vector<int> a(4);
+    rep(i,4) {
+        if(s[i] == "H") a[0]++;
+        else if(s[i] == "2B") a[1]++;
+        else if(s[i] == "3B") a[2]++;
+        else a[3]++;
+    }
+    bool ok = true;
+    rep(i,4) {
+        if(a[i] != 1) {
+            ok = false;
+        }
+    }
+
+    if(ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
+	return 0;
+}
+
+// g++ D.cpp -std=c++11
